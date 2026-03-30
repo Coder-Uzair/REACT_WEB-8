46ef504f75a27183befc4e0df1328dd94d0440e1
Coder-Uzair <githubuzair34@gmail.com>
2026-03-30 12:44:13 -0700

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import PartOne from "./components/PartOne";
+
+export default function App() {
+    return(
+        <>
+        <main>
+            <PartOne/>
+        </main>
+        </>
+    );
+}
---
+++ src/components/PartOne.jsx
@@ -0,0 +1,135 @@
+export default function PartOne() {
+    return(
+        <>
+        <div className="snake1">
+            <div className="a1"><h1><strong>Start <br /><strong>1</strong></strong></h1></div>
+            <div className="b1"><h1><strong>2</strong></h1></div>
+            <div className="c1"><h1><strong>3</strong></h1></div>
+            <div className="d1"><h1><strong>4</strong></h1></div>
+            <div className="e1"><h1><strong>5</strong></h1></div>
+            <div className="f1"><h1><strong>6</strong></h1></div>
+            <div className="g1"><h1><strong>7</strong></h1></div>
+            <div className="h1"><h1><strong>8</strong></h1></div>
+            <div className="i1"><h1><strong>9</strong></h1></div>
+            <div className="j1"><h1><strong>10</strong></h1></div>
+        </div>
+
+        <div className="snake2">
+            <div className="a2"><h1><strong>20</strong></h1></div>
+            <div className="b2"><h1><strong>19</strong></h1></div>
+            <div className="c2"><h1><strong>18</strong></h1></div>
+            <div className="d2"><h1><strong>17</strong></h1></div>
+            <div className="e2"><h1><strong>16</strong></h1></div>
+            <div className="f2"><h1><strong>15</strong></h1></div>
+            <div className="g2"><h1><strong>14</strong></h1></div>
+            <div className="h2"><h1><strong>13</strong></h1></div>
+            <div className="i2"><h1><strong>12</strong></h1></div>
+            <div className="j2"><h1><strong>11</strong></h1></div>
+        </div>
+
+        <div className="snake3">
+            <div className="a3"><h1><strong>21</strong></h1></div>
+            <div className="b3"><h1><strong>22</strong></h1></div>
+            <div className="c3"><h1><strong>23</strong></h1></div>
+            <div className="d3"><h1><strong>24</strong></h1></div>
+            <div className="e3"><h1><strong>25</strong></h1></div>
+            <div className="f3"><h1><strong>26</strong></h1></div>
+            <div className="g3"><h1><strong>27</strong></h1></div>
+            <div className="h3"><h1><strong>28</strong></h1></div>
+            <div className="i3"><h1><strong>29</strong></h1></div>
+            <div className="j3"><h1><strong>30</strong></h1></div>
+        </div>
+
+        <div className="snake4">
+            <div className="a4"><h1><strong>80</strong></h1></div>
+            <div className="b4"><h1><strong>79</strong></h1></div>
+            <div className="c4"><h1><strong>78</strong></h1></div>
+            <div className="d4"><h1><strong>77</strong></h1></div>
+            <div className="e4"><h1><strong>76</strong></h1></div>
+            <div className="f4"><h1><strong>75</strong></h1></div>
+            <div className="g4"><h1><strong>74</strong></h1></div>
+            <div className="h4"><h1><strong>73</strong></h1></div>
+            <div className="i4"><h1><strong>72</strong></h1></div>
+            <div className="j4"><h1><strong>71</strong></h1></div>
+        </div>
+
+        <div className="snake5">
+            <div className="a5"><h1><strong>40</strong></h1></div>
+            <div className="b5"><h1><strong>39</strong></h1></div>
+            <div className="c5"><h1><strong>38</strong></h1></div>
+            <div className="d5"><h1><strong>37</strong></h1></div>
+            <div className="e5"><h1><strong>36</strong></h1></div>
+            <div className="f5"><h1><strong>35</strong></h1></div>
+            <div className="g5"><h1><strong>34</strong></h1></div>
+            <div className="h5"><h1><strong>33</strong></h1></div>
+            <div className="i5"><h1><strong>32</strong></h1></div>
+            <div className="j5"><h1><strong>31</strong></h1></div>
+        </div>
+
+        <div className="snake6">
+            <div className="a6"><h1><strong>60</strong></h1></div>
+            <div className="b6"><h1><strong>59</strong></h1></div>
+            <div className="c6"><h1><strong>58</strong></h1></div>
+            <div className="d6"><h1><strong>57</strong></h1></div>
+            <div className="e6"><h1><strong>56</strong></h1></div>
+            <div className="f6"><h1><strong>55</strong></h1></div>
+            <div className="g6"><h1><strong>54</strong></h1></div>
+            <div className="h6"><h1><strong>53</strong></h1></div>
+            <div className="i6"><h1><strong>52</strong></h1></div>
+            <div className="j6"><h1><strong>51</strong></h1></div>
+        </div>
+
+        <div className="snake7">
+            <div className="a7"><h1><strong>41</strong></h1></div>
+            <div className="b7"><h1><strong>42</strong></h1></div>
+            <div className="c7"><h1><strong>43</strong></h1></div>
+            <div className="d7"><h1><strong>44</strong></h1></div>
+            <div className="e7"><h1><strong>45</strong></h1></div>
+            <div className="f7"><h1><strong>46</strong></h1></div>
+            <div className="g7"><h1><strong>47</strong></h1></div>
+            <div className="h7"><h1><strong>48</strong></h1></div>
+            <div className="i7"><h1><strong>49</strong></h1></div>
+            <div className="j7"><h1><strong>50</strong></h1></div>
+        </div>
+
+        <div className="snake8">
+            <div className="a8"><h1><strong>81</strong></h1></div>
+            <div className="b8"><h1><strong>82</strong></h1></div>
+            <div className="c8"><h1><strong>83</strong></h1></div>
+            <div className="d8"><h1><strong>84</strong></h1></div>
+            <div className="e8"><h1><strong>85</strong></h1></div>
+            <div className="f8"><h1><strong>86</strong></h1></div>
+            <div className="g8"><h1><strong>87</strong></h1></div>
+            <div className="h8"><h1><strong>88</strong></h1></div>
+            <div className="i8"><h1><strong>89</strong></h1></div>
+            <div className="j8"><h1><strong>90</strong></h1></div>
+        </div>
+
+        <div className="snake9">
+            <div className="a9"><h1><strong>Finish <br /><strong>100</strong></strong></h1></div>
+            <div className="b9"><h1><strong>99</strong></h1></div>
+            <div className="c9"><h1><strong>98</strong></h1></div>
+            <div className="d9"><h1><strong>97</strong></h1></div>
+            <div className="e9"><h1><strong>96</strong></h1></div>
+            <div className="f9"><h1><strong>95</strong></h1></div>
+            <div className="g9"><h1><strong>94</strong></h1></div>
+            <div className="h9"><h1><strong>93</strong></h1></div>
+            <div className="i9"><h1><strong>92</strong></h1></div>
+            <div className="j9"><h1><strong>91</strong></h1></div>
+        </div>
+
+        <div className="snake10">
+            <div className="a10"><h1><strong>61</strong></h1></div>
+            <div className="b10"><h1><strong>62</strong></h1></div>
+            <div className="c10"><h1><strong>63</strong></h1></div>
+            <div className="d10"><h1><strong>64</strong></h1></div>
+            <div className="e10"><h1><strong>65</strong></h1></div>
+            <div className="f10"><h1><strong>66</strong></h1></div>
+            <div className="g10"><h1><strong>67</strong></h1></div>
+            <div className="h10"><h1><strong>68</strong></h1></div>
+            <div className="i10"><h1><strong>69</strong></h1></div>
+            <div className="j10"><h1><strong>70</strong></h1></div>
+        </div>
+        </>
+    );
+}
